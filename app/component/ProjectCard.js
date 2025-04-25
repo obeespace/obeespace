79cc5c30c31af2f7a667df259e5ec5e93e764fd6
obeespace <94024823+obeespace@users.noreply.github.com>
2025-04-25 02:41:34 +0100

    best practice

--- app/component/ProjectCard.js
+++ app/component/ProjectCard.js
@@ -1,10 +1,11 @@
+import Image from 'next/image';
 import React from 'react';
 import { IoMdArrowDropright } from "react-icons/io";
 
 const ProjectCard = (props) => {
   return (
     <div className="project-card">
-      <img src={props.picture} alt="Project" className="w-full h-48 object-cover" />
+      <Image src={props.picture} alt="Project" className="w-full h-48 object-cover" />
       <h3 className="text-xl font-bold mt-4">Project Title</h3>
       <div className='flex justify-between mt-3'>
         <p className=" w-fit rounded-xl px-2 py-0.5 flex items-center gap-1 cursor-pointer">Live Link <IoMdArrowDropright/></p>
