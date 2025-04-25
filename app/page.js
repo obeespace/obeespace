254180c21de954a074ed3d4995b1ae3cb1e9114f
obeespace <94024823+obeespace@users.noreply.github.com>
2025-04-25 01:56:54 +0100

    projects page place holder

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import ProjectCard from "./component/ProjectCard";
 
 export default function Home() {
   return (
@@ -8,33 +8,10 @@ export default function Home() {
         <p className="w-3/6">The repayment of our clients trust in delivering above satisfactory results is our mantra</p>
       </div>
 
-      <div className="w-5/6 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-4 mt-10">
-        <div className="">
-          <Image href="" alt="project" width={32} height={32} />
-          <p>project Name</p>
-          <div className="flex justify-between mt-2">
-            <p>Live link</p>
-            <p>Github link</p>
-          </div>
-        </div>
-
-        <div className="">
-          <Image href="" alt="project" width={32} height={32}  />
-          <p>project Name</p>
-          <div className="flex justify-between mt-2">
-            <p>Live link</p>
-            <p>Github link</p>
-          </div>
-        </div>
-
-        <div className="">
-          <Image href="" alt="project" width={32} height={32}  />
-          <p>project Name</p>
-          <div className="flex justify-between mt-2">
-            <p>Live link</p>
-            <p>Github link</p>
-          </div>
-        </div>
+      <div className="w-5/6 mx-auto grid grid-cols-1 lg:grid-cols-3 gap-8 mt-10">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
       </div>
     </main>
   );
